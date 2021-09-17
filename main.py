aacb1df4775216af658dc8e6e3c53b7ebe1dde64
Avani solanki <80258470+Avani6420@users.noreply.github.com>
2021-09-17 13:41:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+    except:
+        pass
 
 
 if __name__ == '__main__':
